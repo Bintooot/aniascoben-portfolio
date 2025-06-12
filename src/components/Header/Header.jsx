8af7ccf430be8a95b03ace57477f4699d959e596
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-06-12 17:51:59 +0800

    feat: adding navlink section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { AlignRight } from "lucide-react";
+import React, { useState } from "react";
+import { AlignRight, X } from "lucide-react";
 import logo from "../../assets/icons/logo.png";
 import "./Header.css";
 
@@ -9,6 +9,34 @@ const Header = () => {
       <div className="header-content">
         <img src={logo} alt="logo.png" />
         <AlignRight style={{ color: "#fff" }} size={30} id="toogle" />
+      </div>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-content">
+          <div className="sidebar-header">
+            <img src={logo} alt="logo.png" width={100} height={100} />
+            <X size={40} id="close" />
+          </div>
+          <ul className="link-wrapper">
+            <li className="link-item">
+              <a href="">Home</a>{" "}
+            </li>
+            <li className="link-item">
+              <a href=""> About Me</a>
+            </li>
+            <li className="link-item">
+              <a href="">Services</a>
+            </li>
+            <li className="link-item">
+              <a href="">Skills</a>
+            </li>
+            <li className="link-item">
+              <a href="">Projects</a>
+            </li>
+            <li className="link-item">
+              <a href=""> Contact Me</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
